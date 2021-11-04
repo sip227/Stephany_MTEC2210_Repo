0ac5dcf474541ab40a158dc6436966e021c46d82
sip227 <stephany.pena@mail.citytech.cuny.edu>
2021-11-04 17:11:09 -0400

    Updates#2
    Code, sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    public GameObject lilpotatoPrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,47 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            SpawnPiggy();
+        }
     }
+    
+    public void Shoot()
+    {
+        Debug.Log("Shooting");
+    }
+
+    public void SpawnPiggy()
+    {
+        GameObject lilpotato = Instantiate(lilpotatoPrefab, Vector3.zero, Quaternion.identity);
+       // lilpotato.GetComponent<PlayerMovement>().speed = 0;
+        lilpotato.GetComponent<PlayerMovement>().enabled = false;
+    }
+
+
 }
